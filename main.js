3ff2976fffe3748ec1548d5f49a80f9c21a1d12c
Nutchayaporn Buarom <nutchayaporn_b@cmu.ac.th>
2023-06-30 02:55:04 +0700

    3.6

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ const { createApp, ref } = Vue;
 createApp({
   setup() {
     const product = ref("Boots");
+    const image = ref("./assets/images/socks_green.jpg");
     const description = ref("A stylish and comfortable pair of boots.");
+    const link = ref("https://www.camt.cmu.ac.th");
     return {
       product,
       description,
+      image,
+      link,
     };
   },
 }).mount("#app");
