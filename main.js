35124ed57445c4db40fe58d57350fbd424a4380b
Nutchayaporn Buarom <nutchayaporn_b@cmu.ac.th>
2023-06-30 04:24:01 +0700

    6.7

--- main.js
+++ main.js
@@ -7,13 +7,25 @@ createApp({
     const description = ref("A stylish and comfortable pair of boots.");
     const link = ref("https://www.camt.cmu.ac.th");
     const inStock = ref(true);
-    const inventory = ref(0);
-    const onSale = ref(false);
+    const inventory = ref(100);
+    const onSale = ref(true);
     const details = ref(["50% cotton", "30% wool", "20% polyester"]);
     const variants = ref([
-      { id: 2234, color: "green" },
-      { id: 2235, color: "blue" },
+      { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+      { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
     ]);
+    const cart = ref(0);
+    function addToCart() {
+      cart.value += 1;
+    }
+    function updateImage(variantImage) {
+      image.value = variantImage;
+    }
+    const toggleStock = () => {
+      inStock.value = !inStock.value;
+      inventory.value = inStock.value ? 100 : 0;
+    };
+
     return {
       product,
       description,
@@ -24,6 +36,10 @@ createApp({
       onSale,
       details,
       variants,
+      cart,
+      addToCart,
+      updateImage,
+      toggleStock,
     };
   },
 }).mount("#app");
