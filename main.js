586aba585941ba9a6ccd256a6af30de7f8e86da4
Nutchayaporn Buarom <nutchayaporn_b@cmu.ac.th>
2023-06-30 03:24:41 +0700

    4.9

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ createApp({
     const image = ref("./assets/images/socks_green.jpg");
     const description = ref("A stylish and comfortable pair of boots.");
     const link = ref("https://www.camt.cmu.ac.th");
+    const inStock = ref(true);
+    const inventory = ref(0);
+    const onSale = ref(false);
     return {
       product,
       description,
       image,
       link,
+      inStock,
+      inventory,
+      onSale,
     };
   },
 }).mount("#app");
