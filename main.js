c0c037cb739b0367e3268b8b6e22abd9c9c2fed0
Nutchayaporn Buarom <nutchayaporn_b@cmu.ac.th>
2023-06-30 03:41:04 +0700

    5.5

--- main.js
+++ main.js
@@ -9,6 +9,11 @@ createApp({
     const inStock = ref(true);
     const inventory = ref(0);
     const onSale = ref(false);
+    const details = ref(["50% cotton", "30% wool", "20% polyester"]);
+    const variants = ref([
+      { id: 2234, color: "green" },
+      { id: 2235, color: "blue" },
+    ]);
     return {
       product,
       description,
@@ -17,6 +22,8 @@ createApp({
       inStock,
       inventory,
       onSale,
+      details,
+      variants,
     };
   },
 }).mount("#app");
