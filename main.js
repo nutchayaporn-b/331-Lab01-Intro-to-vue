f1ce72950586c11c71f764307eb2b8fab72be570
Nutchayaporn Buarom <nutchayaporn_b@cmu.ac.th>
2023-06-30 02:33:14 +0700

    2.5

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const { createApp, ref } = Vue;
 createApp({
   setup() {
     const product = ref("Boots");
+    const description = ref("A stylish and comfortable pair of boots.");
     return {
       product,
+      description,
     };
   },
 }).mount("#app");
